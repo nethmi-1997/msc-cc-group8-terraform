d09eaacab00491fcab845bc8512a830fb08541b5
nethmi-1997 <63659987+nethmi-1997@users.noreply.github.com>
2024-01-02 15:20:52 +0530

    name change

--- s3.tf
+++ s3.tf
@@ -1,4 +1,4 @@
 resource "aws_s3_bucket" "website_bucket" {
-  bucket = "msc-cc-group8-bucket-2"
+  bucket = "msc-cc-group8-bucket-new"
   acl    = "private" # Access Control List (ACL) for the bucket
 }
