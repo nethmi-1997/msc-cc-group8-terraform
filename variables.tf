4d761adcff6fb9e1326c86794faf52c5b33c925d
nethmi-1997 <63659987+nethmi-1997@users.noreply.github.com>
2024-01-02 18:59:10 +0530

    var

--- variables.tf
+++ variables.tf
@@ -11,4 +11,5 @@ variable "codebuild_project_name" {
 variable "github_oauth_token" {
   description = "OAuth token for GitHub access"
   type        = string
+  default = "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"
 }
